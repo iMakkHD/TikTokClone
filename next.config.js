56e195854d718f42ec7679d91e92d871d10308ef
Steven Mak <maksteven2022@gmail.com>
2022-07-21 17:03:29 -0500

    Finished login/logout buttons and added user store/states

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['wallpaperaccess.com'],
+    domains: ['wallpaperaccess.com', 'lh3.googleusercontent.com'],
   }
 }
 
