8ba471269130aafb4c9ebaac0263e0484b5c847b
Steven Mak <maksteven2022@gmail.com>
2022-08-15 15:45:31 -0500

    Upload File Assest functionality

--- pages/upload.tsx
+++ pages/upload.tsx
@@ -3,13 +3,14 @@ import { useRouter } from 'next/router';
 import { FaCloudUploadAlt } from 'react-icons/fa';
 import { MdDelete } from 'react-icons/md';
 import axios from 'axios';
+import { SanityAssetDocument } from '@sanity/client';
 
 import useAuthStore from '../store/authStore';
 import { client } from '../utils/client';
 
 const Upload = () => {
   const [isLoading, setIsLoading] = useState(false);
-  const [videoAsset, setVideoAsset] = useState();
+  const [videoAsset, setVideoAsset] = useState<SanityAssetDocument | undefined>();
   const [wrongFileType, setWrongFileType] = useState(false);
 
   const uploadVideo = async (e: any) => {
@@ -17,7 +18,14 @@ const Upload = () => {
     const fileTypes =['video/mp4', 'video/webm', 'video/ogg'];
 
     if(fileTypes.includes(selectedFile.type)){
-
+      client.assets.upload('file', selectedFile, {
+        contentType: selectedFile.type,
+        filename: selectedFile.name
+      })
+      .then((data) => {
+        setVideoAsset(data);
+        setIsLoading(false);
+      })
     } else{
       setIsLoading(false);
       setWrongFileType(true);
@@ -38,7 +46,13 @@ const Upload = () => {
             <div>
               {videoAsset ? (
                 <div>
-
+                  <video
+                  src={videoAsset.url}
+                  loop
+                  controls
+                  className="rounded-xl h-[450px] mt-16 bg-black"
+                  >
+                  </video>
                 </div>
               ) : (
                 <label className="cursor-pointer">
